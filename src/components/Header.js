18ce5110e3ceceb39fa290482e8c3ea58bd9ce46
AnimeSam <sphung@uncc.edu>
2021-11-09 20:28:49 -0500

    Added check to change margintop depending on the height of the screen. Used react state just for the sake of using it

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,38 @@ import { Box, Center, VStack } from '@chakra-ui/layout'
 import { Text, Heading, Flex } from '@chakra-ui/react'
 import React from 'react'
 
+import { useState } from 'react';
+
+
+
 function Header() {
+    const [headerHeight, setHeaderHeight] = useState()
+    function getHeaderDimensions() {
+        try {
+            const hHeight = document.getElementById('Home').offsetHeight;
+            const height = window.innerHeight
+
+            if (hHeight > height / 3) {
+                console.log(setHeaderHeight)
+                setHeaderHeight("1vh")
+                return headerHeight;
+            }
+            console.log(setHeaderHeight)
+            setHeaderHeight("25vh");
+            return headerHeight;
+        }
+        catch {
+            console.log(setHeaderHeight)
+            setHeaderHeight("1vh");
+            return headerHeight;
+        }
+
+    }
+
+
     return (
-        <Center>
-            <Box id="Home" maxW="1000px" bg="gray" padding="10" marginTop="20em" borderRadius="2xl" style={{ backgroundColor: 'rgba(52, 52, 52, 0.9)' }}>
+        <Center id="Home" marginTop={getHeaderDimensions}>
+            <Box justifyContent="center" maxW="1000px" bg="gray" padding="10" borderRadius="2xl" style={{ backgroundColor: 'rgba(52, 52, 52, 0.9)' }}>
 
                 <VStack className="intro_header">
                     <Flex flexDir="row">
